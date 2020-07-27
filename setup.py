633c338428302274cf9625379925a9af3041a9f1
zulan <git@zulan.net>
2020-07-27 20:31:09 +0200

    put project name in civ4-mp namespace
    I'm not touching the package name for now. It is used only internally anyway. Unfortunately there's no definite definite project/package naming guideline (PEP 423 is still deferred).

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="civpb_watchdog",
+    name="civ4-mp.pb-watchdog",
     version="2.0.0",
     author="Olaf S., Zulan",
     python_requires=">=3.7",
